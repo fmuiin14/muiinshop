e53887de266cf6184f1d88d50aa8861c3af6a356
fmuiin14 <fmuiin14@gmail.com>
2023-01-30 11:21:49 +0700

    mecah dashboard user then alamat

---
+++ resources/views/user/layouts/master.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>@yield('title')</title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('be/user/modules/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('be/user/modules/fontawesome/css/all.min.css') }}">
+
+  <!-- CSS Libraries -->
+  <link rel="stylesheet" href="{{ asset('be/user/modules/jqvmap/dist/jqvmap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('be/user/modules/summernote/summernote-bs4.css') }}">
+  <link rel="stylesheet" href="{{ asset('be/user/modules/owlcarousel2/dist/assets/owl.carousel.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('be/user/modules/owlcarousel2/dist/assets/owl.theme.default.min.css') }}">
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('be/user/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('be/user/css/components.css') }}">
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA --></head>
+
+<body>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+      <nav class="navbar navbar-expand-lg main-navbar">
+        <form class="form-inline mr-auto">
+          <ul class="navbar-nav mr-3">
+            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+            <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+          </ul>
+          <div class="search-element">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search" data-width="250">
+            <button class="btn" type="submit"><i class="fas fa-search"></i></button>
+            <div class="search-backdrop"></div>
+          </div>
+        </form>
+        <ul class="navbar-nav navbar-right">
+
+          <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
+            <div class="dropdown-menu dropdown-list dropdown-menu-right">
+              <div class="dropdown-header">Notifications
+                <div class="float-right">
+                  <a href="#">Mark All As Read</a>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+            <img alt="image" src="{{ asset('be/user/img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
+            <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
+            <div class="dropdown-menu dropdown-menu-right">
+              <div class="dropdown-title">Logged in 5 min ago</div>
+              <a href="features-profile.html" class="dropdown-item has-icon">
+                <i class="far fa-user"></i> Profile
+              </a>
+              <a href="features-activities.html" class="dropdown-item has-icon">
+                <i class="fas fa-bolt"></i> Activities
+              </a>
+              <a href="features-settings.html" class="dropdown-item has-icon">
+                <i class="fas fa-cog"></i> Settings
+              </a>
+              <div class="dropdown-divider"></div>
+              <a href="#" class="dropdown-item has-icon text-danger">
+                <i class="fas fa-sign-out-alt"></i> Logout
+              </a>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <div class="main-sidebar sidebar-style-2">
+            @include('user.layouts.sidebar')
+      </div>
+
+      <!-- Main Content -->
+      <div class="main-content">
+        @yield('content')
+      </div>
+      <footer class="main-footer">
+        <div class="footer-left">
+          Copyright &copy; <?= date('Y') ?> <div class="bullet"></div> <a href="#">Ranura Hijab</a>
+        </div>
+        <div class="footer-right">
+
+        </div>
+      </footer>
+    </div>
+  </div>
+
+  <!-- General JS Scripts -->
+  <script src="{{ asset('be/user/modules/jquery.min.js') }}"></script>
+  <script src="{{ asset('be/user/modules/popper.js') }}"></script>
+  <script src="{{ asset('be/user/modules/tooltip.js') }}"></script>
+  <script src="{{ asset('be/user/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('be/user/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+  <script src="{{ asset('be/user/modules/moment.min.js') }}"></script>
+  <script src="{{ asset('be/user/js/stisla.js') }}"></script>
+
+  <!-- JS Libraies -->
+  <script src="{{ asset('be/user/modules/jquery.sparkline.min.js') }}"></script>
+  <script src="{{ asset('be/user/modules/chart.min.js') }}"></script>
+  <script src="{{ asset('be/user/modules/owlcarousel2/dist/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('be/user/modules/summernote/summernote-bs4.js') }}"></script>
+  <script src="{{ asset('be/user/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+
+  <!-- Page Specific JS File -->
+  {{-- <script src="{{ asset('be/user/js/page/index.js') }}"></script> --}}
+
+  <!-- Template JS File -->
+  <script src="{{ asset('be/user/js/scripts.js') }}"></script>
+  <script src="{{ asset('be/user/js/custom.js') }}"></script>
+
+  {{-- custom JS --}}
+  @yield('js')
+</body>
+</html>
---
+++ resources/views/user/layouts/sidebar.blade.php
@@ -0,0 +1,31 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+      <a href="index.html">Stisla</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+      <a href="index.html">St</a>
+    </div>
+    <ul class="sidebar-menu">
+      <li><a class="nav-link" href="{{ route('admin-dashboard-index') }}"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+      {{-- <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Products</span></a>
+        <ul class="dropdown-menu" style="display: none;">
+          <li><a class="nav-link" href="{{ route('masterProduct.index') }}">Master Product</a></li>
+          <li><a class="nav-link" href="{{ route('product.index') }}">Product Satuan</a></li>
+        </ul>
+      </li> --}}
+      <li><a class="nav-link" href="{{ route('product.index') }}"><i class="far fa-file-alt"></i> <span>Profile</span></a></li>
+      <li><a class="nav-link" href="{{ route('product.index') }}"><i class="far fa-file-alt"></i> <span>Orders</span></a></li>
+      {{-- <li><a class="nav-link" href="{{ route('category.index') }}"><i class="far fa-file-alt"></i> <span>Category</span></a></li>
+      <li><a class="nav-link" href="{{ route('coupon.index') }}"><i class="far fa-file-alt"></i> <span>Coupons</span></a></li>
+      <li><a class="nav-link" href="{{ route('brand.index') }}"><i class="far fa-file-alt"></i> <span>Brands</span></a></li>
+      <li><a class="nav-link" href="{{ route('slider.index') }}"><i class="far fa-file-alt"></i> <span>Slider</span></a></li>
+      <li><a class="nav-link" href="#"><i class="far fa-file-alt"></i> <span>Media Manager*</span></a></li>
+      <li><a class="nav-link" href="#"><i class="far fa-file-alt"></i> <span>Banners*</span></a></li>
+      <li><a class="nav-link" href="#"><i class="far fa-file-alt"></i> <span>Shipping*</span></a></li>
+      <li><a class="nav-link" href="#"><i class="far fa-file-alt"></i> <span>Orders*</span></a></li>
+      <li><a class="nav-link" href="#"><i class="far fa-file-alt"></i> <span>Reviews*</span></a></li>
+      <li><a class="nav-link" href="#"><i class="far fa-file-alt"></i> <span>Users*</span></a></li>
+      <li><a class="nav-link" href="#"><i class="far fa-file-alt"></i> <span>Settings*</span></a></li> --}}
+    </ul>
+  </aside>
